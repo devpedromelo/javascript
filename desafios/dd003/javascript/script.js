13811616d2b5165ee7582241ec9e80629024ef36
devpedromelo <116653056+devpedromelo@users.noreply.github.com>
2023-02-09 17:43:17 -0300

    terminei o ex

--- desafios/dd003/javascript/script.js
+++ desafios/dd003/javascript/script.js
@@ -9,11 +9,41 @@ function verificar(){
         var sex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'photo')
             if(sex[0].checked) {
-               genero = 'Homem' 
+               genero = 'Homem'
+               if(idade >= 0 && idade < 10) {
+                //crianca
+                img.setAttribute('src', 'imagens/crianca-masc.png')
+               }else if(idade < 22){
+                //jovem
+                img.setAttribute('src', 'imagens/jovem-masc.png')
+               }else if(idade < 50){
+                //adulto
+                img.setAttribute('src', 'imagens/adulto-masc.png')
+               }else{
+                //idoso
+                img.setAttribute('src', 'imagens/idoso-masc.png')
+               }
             }else if(sex[1].checked){
                 genero = 'Mulher'
+                if(idade >= 0 && idade < 10) {
+                    //crianca
+                    img.setAttribute('src', 'imagens/crianca-fem.png')
+                }else if(idade < 22){
+                    //jovem
+                    img.setAttribute('src', 'imagens/jovem-fem.png')
+                }else if(idade < 50){
+                    //adulto
+                    img.setAttribute('src', 'imagens/adulto-fem.png')
+                }else{
+                    //idoso
+                    img.setAttribute('src', 'imagens/idoso-fem.png')
+                }
             }
+            res.style.textAlign = 'center'
             res.innerHTML = `Detectamos ${genero} de ${idade} anos.`
+            res.appendChild(img)
     }
 }
