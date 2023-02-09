620eb54817a016cafb6c17408b4502d88912e630
devpedromelo <116653056+devpedromelo@users.noreply.github.com>
2023-02-09 17:10:03 -0300

    refazendo o exercicio

--- desafios/dd003/javascript/script.js
+++ desafios/dd003/javascript/script.js
@@ -1,26 +1,13 @@
 function verificar(){
-    //variaveis
-    var ano = new Date()
-    var fano = ano.getFullYear()
-    var iano = document.querySelector('input#iano')
-    var fsexo = document.getElementsByName('radsex')
-    var idade = fano - Number(iano.value)
-    var res = document.querySelector('div#result')
-    //se digitar um numero maior
-    if(Number(iano.value) > fano || Number(iano.value) == 0){
-        window.alert('[ERRO] O ano que você digitou é maior que o ano atual, verifique os daods e tente novamente')
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('result')
+    if(fano.value == 0 || fano.value > ano) {
+        window.alert('Verifique os dados e tente novamente.')
     }else{
-    //condição de genero
-        var genero = ''
-            if(fsexo[0].checked){
-                genero = 'masculino'
-            }else{
-                genero = 'feminino'
-            }
-        res.innerHTML = `Detectamos o gênero ${genero} de ${idade} anos!`
-        }
-
-        if(genero == 'masculino' && idade >= 0 && idade <= 10){
-            criancam.style.display = 'block'
-        }
+        var sex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `Idade calculada: ${idade}` 
+    }
 }
