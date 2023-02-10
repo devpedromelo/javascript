6979607ae3de0e93be0be539ddca08d709826f09
devpedromelo <116653056+devpedromelo@users.noreply.github.com>
2023-02-10 16:41:01 -0300

    desafio proprio 01

---
+++ desafios/desafios-proprios/desafio1/javascript/script.js
@@ -0,0 +1,53 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('result')
+    if(fano.value == 0 || fano.value > ano || fano.value <= 1900) {
+        window.alert('Verifique os dados e tente novamente.')
+    }else{
+        var sex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'photo')
+            if(sex[0].checked) {
+               genero = 'Homem'
+               if(idade >= 0 && idade < 10) {
+                //crianca
+                img.setAttribute('src', 'imagens/crianca-masc.png')
+               }else if(idade < 22){
+                //jovem
+                img.setAttribute('src', 'imagens/jovem-masc.png')
+               }else if(idade < 50){
+                //adulto
+                img.setAttribute('src', 'imagens/adulto-masc.png')
+               }else{
+                //idoso
+                img.setAttribute('src', 'imagens/idoso-masc.png')
+               }
+            }else if(sex[1].checked){
+                genero = 'Mulher'
+                if(idade >= 0 && idade < 10) {
+                    //crianca
+                    img.setAttribute('src', 'imagens/crianca-fem.png')
+                }else if(idade < 22){
+                    //jovem
+                    img.setAttribute('src', 'imagens/jovem-fem.png')
+                }else if(idade < 50){
+                    //adulto
+                    img.setAttribute('src', 'imagens/adulto-fem.png')
+                }else{
+                    //idoso
+                    img.setAttribute('src', 'imagens/idoso-fem.png')
+                }
+            }
+            res.style.textAlign = 'center'
+            res.style.height = '325px'
+            res.style.background = 'white'
+            res.style.borderRadius = '8px'
+            res.innerHTML = `Detectamos ${genero} de ${idade} anos.`
+            res.appendChild(img)
+            rodape.style.marginTop = '300px'
+    }
+}
