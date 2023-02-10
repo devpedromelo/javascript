56e820e91c1789d4bec16fee1f0f843d9fd907a2
devpedromelo <116653056+devpedromelo@users.noreply.github.com>
2023-02-10 15:54:51 -0300

    ex06

---
+++ exercicios/exercicios-da-apostila/ex06/script.js
@@ -0,0 +1,26 @@
+var res = window.document.querySelector('div#msg')
+
+function mudar1(){
+    document.body.style.background = 'blue'
+    h1.style.color = 'white'
+}
+
+function mudar2(){
+    document.body.style.background = 'yellow'
+    h1.style.color = 'white'
+}
+
+function mudar3(){
+    document.body.style.background = 'green'
+    h1.style.color = 'white'
+}
+
+function mudar4(){
+    document.body.style.background = 'pink'
+    h1.style.color = 'white'
+}
+
+function resetar(){
+    document.body.style.background = 'white'
+    h1.style.color = 'black'
+}
