22ec08b72506822702b7c49ffd244324a5a5f922
devpedromelo <116653056+devpedromelo@users.noreply.github.com>
2023-02-17 10:47:15 -0300

    js funcoes

--- exercicios/ex009/ex023.js
+++ exercicios/ex009/ex023.js
@@ -3,5 +3,9 @@ valores.sort()
 for(let c in valores){
     console.log(`A posição ${c} tem o valor ${valores[c]}`)
 }
-let pos = valores.indexOf(4)
-console.log(pos)
+let pos = valores.indexOf(8)
+    if(pos == -1){
+        console.log('Número não encontrado.')
+    }else{
+        console.log(`Esse valor se encontra na posição ${pos}`)
+    }
