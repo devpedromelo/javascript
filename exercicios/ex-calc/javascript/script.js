fbf15e3e762997133096801f1cff91d0fe9c46d3
devpedromelo <116653056+devpedromelo@users.noreply.github.com>
2023-03-07 10:08:53 -0300

    configurando os botões CE, C e DEL

--- exercicios/ex-calc/javascript/script.js
+++ exercicios/ex-calc/javascript/script.js
@@ -6,12 +6,39 @@ console.log(currentOperationText)
 const buttons = document.querySelectorAll('#buttons-operation button')
 console.log(buttons)
 
+var currentValue = currentOperationText.innerHTML
+
 //------------------------------EVENTOS----------------------------------
 
 buttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         const value = e.target.innerText
-
-        console.log(value)
+        let arrayNum = [value]
+        console.log(arrayNum)
+        //sumindo sinal de igual
+        if(value == '='){
+            currentOperationText.innerHTML = ''
+        }else if(value == 'CE'){//resetando a operação mais recente
+            currentOperationText.innerHTML = ''
+            currentValue.innerHTML = ''
+        }else if(value == 'C'){ //clear
+            previousOperationText.innerHTML = ''
+            currentOperationText.innerHTML = ''
+            currentValue.innerHTML = ''
+        }else{
+            currentOperationText.innerHTML += value
+        }
+        
+        //del
+        if(value == 'DEL'){
+            arrayNum.pop()
+            currentOperationText.innerHTML = arrayNum
+        }
     })
 })
+
+function equal(){
+    var currentValue = currentOperationText.innerHTML
+    previousOperationText.innerHTML = currentValue
+    currentOperationText.innerHTML = ''
+}
---
+++ exercicios/ex025/ex44.js
@@ -0,0 +1,5 @@
+//------------------ARROW FUNCTION-----------------------
+let arrow = (a,b) => a + b
+
+console.log(arrow(4,6))
+
