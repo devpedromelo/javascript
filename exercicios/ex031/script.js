c0a997f842d8d17f75138c5cc9a3276cf51fddb3
devpedromelo <116653056+devpedromelo@users.noreply.github.com>
2023-03-20 12:13:00 -0300

    mexendo nos exercícios anteriores

--- exercicios/ex031/script.js
+++ exercicios/ex031/script.js
@@ -1,8 +1,10 @@
 const caixa1 = document.querySelector('#caixa1')
-const btn = document.querySelector('#copiar')
+const passando = document.querySelector('#btn')
+const repassando = document.querySelector('#btn2')
+const excluir = document.querySelector('#btn3')
 const caixa2 = document.querySelector('#caixa2')
 const todosCursos = [...document.querySelectorAll('.curso')]
-const selecionadosCaixa2 = []
+let selecionadosCaixa2 = []
 
 todosCursos.map((el)=>{
     el.addEventListener('click',(evt)=>{
@@ -11,13 +13,55 @@ todosCursos.map((el)=>{
     })
 })
 
-function passando(){
-    let todosSelecionados = [document.querySelector('.seletor')]
+passando.addEventListener('click', ()=>{
+    const todosSelecionados = [...document.querySelectorAll('.seletor')]
     todosSelecionados.map((el)=>{
-        caixa2.appendChild(el)
-        selecionadosCaixa2 = [el]
+        if(el in caixa2 == false){
+            caixa2.appendChild(el)
+            el.classList.remove('seletor')
+            selecionadosCaixa2 = [el]
+            console.log(selecionadosCaixa2)
+        }else if(el in caixa2 == true){
+            window.alert('Sai dessa')
+        }
     })
-}
+})
+
+repassando.addEventListener('click', ()=>{
+    const todosSelecionados = [...document.querySelectorAll('.seletor')]
+    todosSelecionados.map((el)=>{
+        if(el in caixa1 == false){
+            caixa1.appendChild(el)
+            el.classList.remove('seletor')
+            selecionadosCaixa2 = [el]
+            console.log(selecionadosCaixa2)
+        }else if(el in caixa1 == true){
+            window.alert('Sai dessa')
+        }
+    })
+})
+
+excluir.addEventListener('click', ()=>{
+    const todosSelecionados = [...document.querySelectorAll('.seletor')]
+    todosSelecionados.map((el)=>{
+        el.remove('seletor')
+    })
+})
+
+
+// function passando(){
+//     let todosSelecionados = [document.querySelector('.seletor')]
+//     todosSelecionados.map((el)=>{
+//         if(el in caixa2 == true){
+//             window.alert('Sai dessa')
+//         }else if(el in caixa2 == false){
+//             caixa2.appendChild(el)
+//             el.classList.remove('seletor')
+//             selecionadosCaixa2 = [el]
+//             console.log(selecionadosCaixa2) 
+//         }
+//     })
+// }
 
 // function repassando(){
 //     const selecionadosCaixa1 = selecionadosCaixa2
