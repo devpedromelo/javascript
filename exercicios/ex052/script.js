54723d53d5c9c2ccfe5173396aae71fd79e22133
devpedromelo <116653056+devpedromelo@users.noreply.github.com>
2023-03-31 18:17:52 -0300

    uptade

--- exercicios/ex052/script.js
+++ exercicios/ex052/script.js
@@ -17,6 +17,7 @@ class luxo extends carro{//classe filho
 let nomeDoCarro = document.querySelector('#nome')
 let portasDoCarro = document.querySelector('#portas')
 let gas = document.querySelector('#gasolina')
+let tecLux = document.querySelector("#lux")
 let tec = document.querySelector("#tec")
 let tipoNormal = document.querySelector("#normal")
 let tipoLuxo = document.querySelector("#luxo")
@@ -25,8 +26,7 @@ let res = document.querySelector(".res")
 
 btn.addEventListener("click", ()=>{
     let verificacao = ''
-    if(tipoNormal.checked){
-        verificacao = 'Carro Normal'
+    verificacao = 'Carro Normal'
         const car = new carro
         car.nome=nomeDoCarro.value
         car.portas = Number(portasDoCarro.value)
@@ -34,7 +34,4 @@ btn.addEventListener("click", ()=>{
         res.innerHTML = `Nome do carro: ${car.nome}<br>`
         res.innerHTML += `Número de portas: ${car.portas}<br>`
         res.innerHTML += `O máximo de litros que ele aguenta: ${car.gasolina}`
-    }else{
-        window.alert("tudo ok")
-    }
 })
