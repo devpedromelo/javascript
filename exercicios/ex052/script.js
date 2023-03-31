3d8f10a2d81e6436c805b10c8c8ab364fd2ff998
devpedromelo <116653056+devpedromelo@users.noreply.github.com>
2023-03-31 17:51:34 -0300

    uptade

--- exercicios/ex052/script.js
+++ exercicios/ex052/script.js
@@ -2,19 +2,7 @@ class carro{ //classe pai
     constructor(nome,portas,gasolina){
         this.nome = nome
         this.portas = portas
-        this.gasolina=gasolina
-        this.ligado = false
-        this.velMax = 0
-        this.cor=undefined
-    }
-    ligar=function(){
-        this.ligado = true
-    }
-    desligar=function(){
-        this.ligado = false
-    }
-    setCor=function(cor){
-        this.cor=cor
+        this.gasolina = gasolina
     }
 }
 
@@ -26,26 +14,27 @@ class luxo extends carro{//classe filho
     }
 }
 
-const carro1 = new carro('Normal',4)
-const carro2 = new luxo('lamborghini', 2, 10, true)
-carro1.ligar()
-carro1.setCor('Preto')
+let nomeDoCarro = document.querySelector('#nome')
+let portasDoCarro = document.querySelector('#portas')
+let gas = document.querySelector('#gasolina')
+let tec = document.querySelector("#tec")
+let tipoNormal = document.querySelector("#normal")
+let tipoLuxo = document.querySelector("#luxo")
+let btn = document.querySelector("#btn")
+let res = document.querySelector(".res")
 
-console.log(`Nome: ${carro1.nome}`)
-console.log(`Portas: ${carro1.portas}`)
-console.log(`Ligado: ${carro1.ligar?'Sim':'Não'}`)
-console.log(`Velocidade: ${carro1.velMax}`)
-console.log(`Cor: ${carro1.cor}`)
-console.log('---------------------------------')
-
-carro2.setCor('Vermelho')
-
-console.log(`Nome: ${carro2.nome}`)
-console.log(`Portas: ${carro2.portas}`)
-console.log(`Ligado: ${carro2.ligado?'Sim':'Não'}`)
-console.log(`Velocidade: ${carro2.velMax}`)
-console.log(`Cor: ${carro2.cor}`)
-console.log(`Tecnologico: ${carro2.tecnologico?'Sim':'Não'}`)
-console.log(`Gasolina: ${carro2.gasolina}L`)
-
-console.log('---------------------------------')
+btn.addEventListener("click", ()=>{
+    let verificacao = ''
+    if(tipoNormal.checked){
+        verificacao = 'Carro Normal'
+        const car = new carro
+        car.nome=nomeDoCarro.value
+        car.portas = Number(portasDoCarro.value)
+        car.gasolina = Number(gas.value)
+        res.innerHTML = `Nome do carro: ${car.nome}<br>`
+        res.innerHTML += `Número de portas: ${car.portas}<br>`
+        res.innerHTML += `O máximo de litros que ele aguenta: ${car.gasolina}`
+    }else{
+        window.alert("tudo ok")
+    }
+})
